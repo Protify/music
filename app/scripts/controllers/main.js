43013b64354377d15b678769314107f9146e514c
ljleppan <leo.leppanen@helsinki.fi>
2014-05-05 12:59:24 +0300

    Project skeleton from Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('protifyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
