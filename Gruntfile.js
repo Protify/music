cf9d17cfe91918dd922c60875181496476578afc
ljleppan <leo.leppanen@helsinki.fi>
2014-05-08 16:56:21 +0300

    Removed bootstrap, replaced necessary parts with singular components

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,7 +286,13 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
-        }]
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'app/bower_components/components-font-awesome/fonts/',
+          src: ['*.*'],
+          dest: '<%= yeoman.dist %>/fonts'
+      }]
       },
       styles: {
         expand: true,
